ca4bc892253470df7c76006feaeba1829ba2ddb0
Roger Pereira <rogerpereira123@gmail.com>
2015-01-26 21:52:56 -0500

    BST operations

--- BST/BST.py
+++ BST/BST.py
@@ -1,4 +1,25 @@
 from Node import Node
+import random
+import queue
+
+##using BFS
+def TraverseBFS(n):
+    q =  queue.Queue()
+    q.put(n)
+    while q.qsize() > 0:
+        curNode = q.get()
+        if curNode.left != None:
+            q.put(curNode.left)
+        if curNode.right != None:
+            q.put(curNode.right)
+        print(str(curNode.data))
+          
+     
+ 
+
+        
+   
+
 
 def IsBST(n, min , max):
     if n == None:
@@ -17,7 +38,31 @@ def IsBST(n, min , max):
     return result
     
 
+def Insert(root, n):
+    if n == None:
+        return
+    if root == None:
+        return 
+    if n.data > root.data:
+        if root.right != None:
+            Insert(root.right , n)
+        else:
+            root.right = n
+            return
+    elif n.data < root.data:
+        if root.left != None:
+            Insert(root.left, n)
+        else:
+            root.left = n
+            return
+    
 
 
-print(IsBST(root , 0 , 100))
 
+
+r = random.sample(range(1, 200) , 2)
+root = Node(50 , None, None)
+for i in r:
+    Insert(root ,Node(i, None , None))
+
+TraverseBFS(root)
